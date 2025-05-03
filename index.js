fe28b08d530f610ce1677456fa6be7ff8184f5cd
Alexandru Branza <branza.alex@gmail.com>
2025-05-03 20:45:59 +0300

    mkv parser

---
+++ index.js
@@ -0,0 +1,9 @@
+var MatroskaRead = require('./lib/read');
+var MatroskaModels = require('./lib/model');
+
+var MatroskaParser = {
+    Read: MatroskaRead,
+    Model: MatroskaModels
+};
+
+module.exports = MatroskaParser;
